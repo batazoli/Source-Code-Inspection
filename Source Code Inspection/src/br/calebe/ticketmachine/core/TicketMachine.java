78ad939228f43efac0f2121bbab49163e81beb52
Giovana Ellero <112666623+GikaEllero@users.noreply.github.com>
2024-04-14 20:26:54 -0300

    Update TicketMachine.java
    Corrigido erros em relação a função getTroco e ao saldo da maquina.

--- Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
@@ -2,7 +2,10 @@ package br.calebe.ticketmachine.core;
 
 import br.calebe.ticketmachine.exception.PapelMoedaInvalidaException;
 import br.calebe.ticketmachine.exception.SaldoInsuficienteException;
+
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 
 /**
  *
@@ -13,16 +16,19 @@ public class TicketMachine {
     protected int valor;
     protected int saldo;
     protected int[] papelMoeda = {2, 5, 10, 20, 50, 100};
+    protected Map<PapelMoeda> saldoMaquina;
 
-    public TicketMachine(int valor) {
+    public TicketMachine(int valor, HashMap saldoMaquina) {
         this.valor = valor;
         this.saldo = 0;
+        this.saldoMaquina = saldoMaquina;
     }
 
     public void inserir(int quantia) throws PapelMoedaInvalidaException {
         boolean achou = false;
         for (int i = 0; i < papelMoeda.length && !achou; i++) {
             if (papelMoeda[1] == quantia) {
+                saldoMaquina.replace(papelMoeda[i], saldoMaquina.get(papelMoeda[i]).quantidade + 1);
                 achou = true;
             }
         }
@@ -36,19 +42,27 @@ public class TicketMachine {
         return saldo;
     }
 
-    public Iterator<Integer> getTroco() {
+    public Iterator<PapelMoeda> getTroco() {
         Iterator trocoIterator = new Troco(saldo).getIterator();
-        int troco = 0;
+        PapelMoeda troco = null;
 
-        while (trocoIterator.hasNext()) {
+        if (trocoIterator.hasNext()) {
             PapelMoeda retorno = trocoIterator.next();
-            troco += retorno.quantidade * retorno.valor;
+
+            if(saldoMaquina.get(retorno.valor).quantidade < retorno.quantidade){
+                throw new TrocoInsuficienteException();
+            }
+
+            saldoMaquina.replace(retorno.valor, saldoMaquina.get(retorno.valor).quantidade - retorno.quantidade);
+            
+            troco = retorno;
         }
 
         if(troco != this.saldo)
             throw new TrocoInsuficienteException();
 
-        this.saldo -= troco;
+        if(troco != null)
+            this.saldo -= troco.quantidade * troco.valor;
 
         return troco;
     }
