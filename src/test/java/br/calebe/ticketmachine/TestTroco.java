ef72cb186dfae64353bc4f014c08f6da7092ed2f
Giovana Ellero <112666623+GikaEllero@users.noreply.github.com>
2024-04-24 21:55:59 -0300

    Adicionado testes do troco

--- src/test/java/br/calebe/ticketmachine/TestTroco.java
+++ src/test/java/br/calebe/ticketmachine/TestTroco.java
@@ -8,10 +8,21 @@ import br.calebe.ticketmachine.core.*;
 public class TestTroco {
     @Test
     public void TestHasNext() throws Throwable{
-        //Iterator<PapelMoeda> underTest = new Troco(55).getIterator();
+        Iterator<PapelMoeda> underTest = new Troco(55).getIterator();
 
-        //boolean result = underTest.hasNext();
+        boolean result = underTest.hasNext();
 
-        //assertEquals(true, result);
+        assertEquals(true, result);
+    }
+
+    @Test
+    public void TestNext() throws Throwable{
+        Iterator<PapelMoeda> underTest = new Troco(55).getIterator();
+
+        PapelMoeda result = underTest.next();
+
+        PapelMoeda esperado = new PapelMoeda(50, 1);
+
+        assertEquals(esperado, result);
     }
 }
